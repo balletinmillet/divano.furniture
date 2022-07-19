803f231c0f2aaca7542a6e91ea32cc0ded2ac8f3
sciper <alljefferson8558@gmail.com>
2022-07-19 17:38:33 +0300

    add admin pannel

--- app/Http/Controllers/CatalogController.php
+++ app/Http/Controllers/CatalogController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers;
 
 use App\Models\Brand;
 use App\Models\Category;
+use App\Models\Product;
 use App\Tools\Template;
 use Illuminate\Http\Request;
 
@@ -18,8 +19,15 @@ class CatalogController extends Controller
 
     public function category($code)
     {
-        $category = Category::where('code', $code)->first();
+        $category = Category::where('code', $code)->firstOrFail();
         $lastVisitedProducts = $category->products;
         return view(Template::route() .'catalog.category', compact('category', 'lastVisitedProducts'));
     }
+
+    public function product($category, $productCode)
+    {
+        $product = Product::where('code', $productCode)->firstOrFail();
+        return view(Template::route() .'catalog.product', compact('product'));
+    }
+
 }
--- routes/web.php
+++ routes/web.php
@@ -1,9 +1,10 @@
 <?php
 
-use App\Http\Controllers\CatalogController;
-use App\Http\Controllers\IndexController;
-use Illuminate\Support\Facades\Route;
 
+use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\IndexController;
+use App\Http\Controllers\CatalogController;
+use App\Http\Controllers\Admin\IndexController as AdminIndexController;
 
 Route::get('/', IndexController::class)->name('index');
 
@@ -15,9 +16,22 @@ Route::group([
     'as' => 'catalog.', // имя маршрута, например catalog.index
     'prefix' => 'catalog', // префикс маршрута, например catalog/index
 ], function () {
-
     Route::get('/', [CatalogController::class, 'index'])->name('index'); // главная страница каталога
     Route::get('{category}', [CatalogController::class, 'category'])->name('category'); // категория каталога товаров
+    Route::get('{category}/{product}', [CatalogController::class, 'product'])->name('product'); // категория каталога товаров
+});
+
+/*
+ * Панель управления магазином для администратора сайта
+ */
+Route::group([
+    'as' => 'admin.', // имя маршрута, например admin.index
+    'prefix' => 'admin', // префикс маршрута, например admin/index
+    //'namespace' => 'Admin', // пространство имен контроллера
+    //'middleware' => ['auth', 'admin'] // один или несколько посредников
+], function () {
+    // главная страница панели управления
+    Route::get('index', AdminIndexController::class)->name('index');
 });
 
 
