88b811b804240986fc15893f926fdb168d0e292e
sciper <alljefferson8558@gmail.com>
2022-07-24 16:19:33 +0300

    add lang

---
+++ resources/lang/ru/admin/common.php
@@ -0,0 +1,21 @@
+<?php
+return [
+    'form' => [
+        'name' => 'Название',
+        'code' => 'Код',
+        'parentCategory' => 'Родительская категория',
+        'upperLevel' => 'Верхний уровень',
+        'image' => 'Изображение',
+        'imageType' => 'png, jpeg',
+        'categoryDescription' => 'Описание категории'
+    ],
+    'button' => [
+        'open' => 'Открыть',
+        'edit' => 'Редактировать',
+        'delete' => 'Удалить',
+        'save' => 'Сохранить'
+    ],
+    'question' => [
+        'categoryDelete' => 'Удалить эту категорию?'
+    ],
+];
---
+++ resources/lang/ru/app/controllers.php
@@ -0,0 +1,13 @@
+<?php
+return [
+    'admin' => [
+        'category' => [
+            'successCreated' => 'Новая категория успешно создана',
+            'successUpdated' => 'Категория была изменена',
+            'successDeleted' => 'Категория успешно удалена',
+            'errorDeletedBecauseChildren' => 'Нельзя удалить категорию с дочерними категориями',
+            'errorDeletedBecauseProducts' => 'Нельзя удалить категорию, которая содержит товары',
+        ]
+    ]
+];
+
